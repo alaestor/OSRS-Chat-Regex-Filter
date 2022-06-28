fc67ed95b2eb77ab366d1e5aba329fd15431fc33
Alaestor <alaestor@null.net>
2022-06-28 09:22:39 -0400

    logging, output, settings.

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os
 import re
+import sys
 import unicodedata
+import logging
 from typing import List
 
 def remove_accents(string: str) -> str:
@@ -35,43 +37,76 @@ def test_regex_against_samples(
 		directory: os.path,
 		regex_filename: str,
 		sample_filename: str,
-		detailed: bool = False
+		stop_on_fail: bool = False,
 		) -> List[str] and bool and float:
 	patterns = regex_patterns_from_file(os.path.join(directory, regex_filename))
 	detections = []
+	logging.debug("Testing " + directory)
 	for line in readlines(os.path.join(directory, sample_filename)):
 		detections.append(0)
 		sample = remove_accents(line)
 		for pattern in patterns:
 			if pattern.search(sample):
 				detections[-1] += 1
+		if not detections[-1] > 0:
+			logging.warning("Failed to match:\n" + sample)
+			if stop_on_fail:
+				raise Exception("A test failed and 'stop_on_fail' was True")
 	if len(detections) > 0:
 		percent = sum(detections) / len(detections)
 		success = all(n >= 1 for n in detections) == True
-	else:
-		percent = 0.0
-		success = False
-	return [p.pattern + "\n" for p in patterns], success, percent
+	return [p.pattern + "\n" for p in patterns], success or 0.0, percent or False
 
-def build(directory: os.path, stop_on_fail: bool = False):
+def build(
+		directory: os.path,
+		output_filename: os.path = None,
+		stop_on_fail: bool = False,
+		print_collated_regex_string: bool = False):
 	r = "regex.txt"
 	s = "samples.txt"
-	print("\nTesting...\n")
+	logging.debug(f"Testing in directory '{directory}'")
 	regex_strings = []
 	for folder in subfolders_containing(directory, [r, s]):
-		patterns, passing, percent = test_regex_against_samples(folder, r, s)
+		patterns, passing, percent = test_regex_against_samples(folder, r, s, stop_on_fail)
 		regex_strings.extend(patterns)
-		state = f"{'[Success]' if passing else '[FAILED]'}"
-		print(f"{state:10} {percent:4.0%} detection for {folder}")
-		if (percent < 1.0 and stop_on_fail):
-			raise Exception("A test failed")
-	print("\nBuilding...\n")
-	print("".join(regex_strings))
-	print("Finished Building.")
+		state = f"{'Success:' if passing else 'FAILED:'}"
+		msg = f"{state:8} {percent:4.0%} detection for '{folder[len(directory)+1:]}'"
+		if passing:
+			logging.info(msg)
+		else:
+			logging.error(msg)
+	collated_regex_string = "".join(regex_strings)
+	logging.info("Collated regular expressions.")
+	if output_filename:
+		with open(output_filename, mode="w") as output_file:
+			output_file.write(collated_regex_string)
+		logging.info("Output collated regular expressions to " + output_filename)
+	if print_collated_regex_string:
+		print("\n" + collated_regex_string)
 
 if __name__ == "__main__":
+	# todo: have optional argv
+	log_level = logging.INFO
+	log_file = __file__ + ".log"
+	output_file = "output.regex.txt"
+	cwd = "regex"
+	stop_on_fail = False
+	print_collated_regex_string = True
+
 	try:
-		cwd = "regex"
-		build(cwd)
+		logging.basicConfig(
+			level=log_level,
+			format="[%(levelname)s] %(message)s",
+			encoding='UTF-8',
+			handlers=[
+				logging.FileHandler(log_file, encoding="UTF-8", mode="w"),
+				logging.StreamHandler(sys.stdout)
+			]
+		)
+		build(
+			cwd,
+			output_file,
+			stop_on_fail,
+			print_collated_regex_string)
 	except Exception as e:
-		print(e)
+		logging.critical(str(e))
