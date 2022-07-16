180137c34bd5c8bd7a075f23b9ac532d5e7830ee
Alaestor <alaestor@null.net>
2022-07-15 22:40:13 -0400

    item editing

--- gui.py
+++ gui.py
@@ -69,10 +69,10 @@ class EditableTreeview(tk.Frame):
 		def __init__(self, parent, multi_select):
 			tk.Menu.__init__(self, parent, tearoff=0)
 			edit_state = "normal" if not multi_select else "disabled"
-			self.add_command(label="Edit", state=edit_state)
+			self.add_command(label="Edit", state=edit_state, command=parent.event_item_edit)
 			self.add_command(label="Create", state=edit_state)
 			self.add_separator()
-			self.add_cascade(label="Export Regex", menu=parent.exportmenu)
+			self.add_cascade(label="Export Regex", menu=parent.export_menu)
 			self.add_separator()
 			self.add_command(label="Remove")
 
@@ -97,7 +97,7 @@ class EditableTreeview(tk.Frame):
 		self.tree.configure(yscrollcommand=self.vsb.set)
 		self.vsb.pack(side="right", fill="y")
 		self.tree.pack(side="top", fill="both", expand=True)
-		self.exportmenu = ExportMenu(self)
+		self.export_menu = ExportMenu(self)
 
 		tree_event_bindings = {
 			"<Double-1>": self.event_item_edit,
@@ -112,11 +112,11 @@ class EditableTreeview(tk.Frame):
 	def event_update_selection(self, event):
 		logging.debug(f"{event} {self.tree.selection()} {self.tree.focus()}")
 
-	def event_deselect(self, event):
+	def event_deselect(self, event=None):
 		logging.debug("treeview selection cleared")
 		self.tree.selection_remove(self.tree.selection())
 
-	def event_select_all(self, event):
+	def event_select_all(self, event=None):
 		def recurse(node=None):
 			l = [] if node == None else [node]
 			for child in self.tree.get_children(node):
@@ -124,9 +124,47 @@ class EditableTreeview(tk.Frame):
 			return l
 		self.tree.selection_add(recurse())
 
-	def event_item_edit(self, event):
+	def event_item_edit(self, event=None):
 		item = self.tree.selection()[0] # now you got the item on that tree
-		print("you clicked on", self.tree.item(item,"text"))
+		item_text = self.tree.item(item,"text")
+		logging.debug(f"editing tree item '{item_text}'")
+
+		def edit_item_text(
+				cb_accept,
+				cb_cancel=None,
+				x=0, relx=0,
+				y=0, rely=0
+			):
+			inputbox = tk.Entry(self, borderwidth=0, highlightthickness=1)
+			inputbox.insert(0, item_text)
+			inputbox.selection_from(0)
+			inputbox.selection_to("end")
+			inputbox.place(x=x, relx=relx, rely=rely, y=y, relwidth=0.9, width=-1)
+			inputbox.focus_set()
+			inputbox.grab_set()
+
+			def accept_edit(e):
+				if cb_accept is not None:
+					cb_accept(e.widget.get())
+				e.widget.destroy()
+
+			def cancel_edit(e):
+				if cb_cancel is not None:
+					cb_cancel()
+				e.widget.destroy()
+
+			inputbox.bind("<Return>", accept_edit)
+			inputbox.bind("<Escape>", cancel_edit)
+
+		def user_input(input_string):
+			if input_string == "":
+				logging.debug(f"deleted item '{item_text}' (blank input)")
+				self.tree.delete(item)
+			else:
+				logging.debug(f"changed item '{item_text}' to '{input_string}'")
+				self.tree.item(item, text=input_string)
+
+		edit_item_text(user_input, y=self.tree.bbox(item)[1])
 
 """
 list of valid strings which shouldn't match any patterns
